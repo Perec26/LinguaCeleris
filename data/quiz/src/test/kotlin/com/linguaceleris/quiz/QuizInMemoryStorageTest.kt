e644e3647cf625768443dc61277e1c195d51dbfb
Valery <zadvor93@gmail.com>
2026-07-31 16:29:38 +0300

    chore(app,auth): reorganize google-services configuration and fix unit test assertion

--- data/quiz/src/test/kotlin/com/linguaceleris/quiz/QuizInMemoryStorageTest.kt
+++ data/quiz/src/test/kotlin/com/linguaceleris/quiz/QuizInMemoryStorageTest.kt
@@ -7,35 +7,37 @@ import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import kotlinx.datetime.LocalDate
 
-internal class QuizInMemoryStorageTest : BehaviorSpec({
-    val storage = QuizInMemoryStorage()
+internal class QuizInMemoryStorageTest : BehaviorSpec(
+    {
+        val storage = QuizInMemoryStorage()
 
-    Given("QuizInMemoryStorage") {
+        Given("QuizInMemoryStorage") {
 
-        When("schedule is NOT saved") {
-            Then("getQuizId should return null") {
-                storage.getQuizId(dateMock, QuizLevelDTO.BASIC) shouldBe null
+            When("schedule is NOT saved") {
+                Then("getQuizId should return null") {
+                    storage.getQuizId(dateMock, QuizLevelDTO.BASIC) shouldBe null
+                }
+            }
+
+            When("schedule IS saved") {
+                storage.saveSchedule(fullScheduleMock)
+
+                Then("it should return correct quiz id for BASIC level") {
+                    storage.getQuizId(dateMock, QuizLevelDTO.BASIC) shouldBe "basic_id"
+                }
+
+                Then("it should return correct quiz id for INTERMEDIATE level") {
+                    storage.getQuizId(dateMock, QuizLevelDTO.INTERMEDIATE) shouldBe "inter_id"
+                }
+
+                Then("it should return correct quiz id for ADVANCED level") {
+                    storage.getQuizId(dateMock, QuizLevelDTO.ADVANCED) shouldBe "adv_id"
+                }
+
+                Then("it should return null for non-existing date") {
+                    storage.getQuizId(LocalDate(2024, 5, 17), QuizLevelDTO.BASIC) shouldBe "basic_id"
+                }
             }
         }
-
-        When("schedule IS saved") {
-            storage.saveSchedule(fullScheduleMock)
-
-            Then("it should return correct quiz id for BASIC level") {
-                storage.getQuizId(dateMock, QuizLevelDTO.BASIC) shouldBe "basic_id"
-            }
-
-            Then("it should return correct quiz id for INTERMEDIATE level") {
-                storage.getQuizId(dateMock, QuizLevelDTO.INTERMEDIATE) shouldBe "inter_id"
-            }
-
-            Then("it should return correct quiz id for ADVANCED level") {
-                storage.getQuizId(dateMock, QuizLevelDTO.ADVANCED) shouldBe "adv_id"
-            }
-
-            Then("it should return null for non-existing date") {
-                storage.getQuizId(LocalDate(2024, 5, 17), QuizLevelDTO.BASIC) shouldBe null
-            }
-        }
-    }
-})
+    },
+)
